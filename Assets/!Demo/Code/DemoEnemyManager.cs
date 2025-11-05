4860459f2e5156726bb299cf939946348ef7d8cb
Gr4tte <johanvwr@gmail.com>
2025-11-05 07:34:51 +0100

    Demo Jam 11-05

---
+++ Assets/!Demo/Code/DemoEnemyManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace UnityTemplate
+{
+    public class DemoEnemyManager : Singleton<DemoEnemyManager>
+    {
+        [SerializeField] private DemoEnemyDatabase _enemyDatabase;
+        [SerializeField] private Vector3 _position;
+        
+        public void SpawnEnemy(DemoEnemyEnum type)
+        {
+            if (!_enemyDatabase.TryGet(type, out GameObject enemyPrefab)) return;
+            Instantiate(enemyPrefab, _position, Quaternion.identity);
+
+            GameObject enemyPrefab2 = _enemyDatabase.Get(type);
+            if (!enemyPrefab2) return;
+            Instantiate(enemyPrefab2, _position, Quaternion.identity);
+        }
+    }
+}
